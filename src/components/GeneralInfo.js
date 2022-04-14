e2b98c7f17841283f8ba2d0732fbaf1ff400b4df
bea <chamoischamois@protonmail.com>
2022-04-14 14:47:46 +0800

    Added components folder

---
+++ src/components/GeneralInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class GeneralInfo extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { props }
+    }
+}
+
+export default GeneralInfo;
