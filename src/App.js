19826c01710607d0a517d9839c1ddb80caf16f0a
bea <chamoischamois@protonmail.com>
2022-04-14 14:48:26 +0800

    Setup basic form structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      general: {
+        name: '',
+        email: '',
+        phone: ''
+      },
+      education: {
+        school: '',
+        studyField: '',
+        gradYear: ''
+      },
+      work: {
+        company: '',
+        position: '',
+        jobScope: '',
+        dateStart: '',
+        dateEnd: ''
+      }
+    }
+  }
+
+  render(){
   return (
-    <div>
+    <div className='form-container'>
+      <form onSubmit={handleOnSubmit}>
+        <button>Submit</button>
+      </form>
     </div>
   );
+  }
 }
 
 export default App;
